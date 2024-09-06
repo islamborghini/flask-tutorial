2f9df36592a2416c596a58771b66463f4be67ad3
Islam Assanov <islamassanov66@gmail.com>
2024-09-06 19:34:58 +0800

    Templates

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'islamborghini'}
+    return render_template('index.html', title='Home', user=user)
